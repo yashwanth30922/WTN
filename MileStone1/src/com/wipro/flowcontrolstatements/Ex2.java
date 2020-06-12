ee78bbdfef6bc9ec66cfb81a4d5c6855fe9a6250
lenovo <lenovo@DESKTOP-LGP4QD4>
2020-06-12 08:31:59 +0530

    this is second problem in flow control statemnts

---
+++ MileStone1/src/com/wipro/flowcontrolstatements/Ex2.java
@@ -0,0 +1,13 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex2 {
+public static void main(String[] args) {
+	int n=Integer.parseInt(args[0]);
+	if(n%2==0) {
+		System.out.println("even");
+	}
+	else {
+		System.out.println("odd");
+	}
+}
+}
