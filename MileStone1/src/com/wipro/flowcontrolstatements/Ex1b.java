e5865bd1151efc8db534a252d2a1802df0ca169c
lenovo <lenovo@DESKTOP-LGP4QD4>
2020-06-12 08:25:03 +0530

    this is first problem in flow cotrol statements

---
+++ MileStone1/src/com/wipro/flowcontrolstatements/Ex1b.java
@@ -0,0 +1,14 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex1b {
+public static void main(String[] args) {
+	int a=Integer.parseInt(args[0]);
+	int b=Integer.parseInt(args[1]);
+	if(a==b) {
+		System.out.println("true");
+	}
+	else {
+		System.out.println("false");
+	}
+}
+}
