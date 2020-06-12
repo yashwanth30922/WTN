4d1940c4d40e2d4c50e2ff21f4fb64499dc9f038
lenovo <lenovo@DESKTOP-LGP4QD4>
2020-06-12 08:08:12 +0530

    this is third problem in language basics

---
+++ MileStone1/src/com/wipro/languagebasics/Ex3.java
@@ -0,0 +1,9 @@
+package com.wipro.languagebasics;
+
+public class Ex3 {
+public static void main(String[] args) {
+	int a=Integer.parseInt(args[0]);
+	int b=Integer.parseInt(args[1]);
+	System.out.println(a+b);
+}
+}
